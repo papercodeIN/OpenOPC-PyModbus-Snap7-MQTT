2c0578ee1e6ce7866e1c6f39755b8770b4e6a105
Fusion Automate <eng.innovativ@gmail.com>
2023-04-07 11:51:52 +0530

    Update MQTT_Publisher.py

--- Raspberry PI Pico W/MQTT/MQTT_Publisher.py
+++ Raspberry PI Pico W/MQTT/MQTT_Publisher.py
@@ -25,7 +25,6 @@ mqtt_host = ""
 mqtt_username = ""  # Your Adafruit IO username
 mqtt_password = ""  # Adafruit IO Key
 mqtt_publish_topic = ""  # The MQTT topic for your Adafruit IO Feed
-mqtt_receive_topic = ""  # The MQTT topic for your Adafruit IO Feed
 
 # Enter a random ID for this MQTT Client
 # It needs to be globally unique across all of Adafruit IO.
@@ -54,14 +53,6 @@ def mqtt_subscription_callback(topic, message):
 mqtt_client.set_callback(mqtt_subscription_callback)
 mqtt_client.connect()
 
-# Set the initial state of the LED to off, and let the MQTT topic know about it
-led.value(0)
-mqtt_client.publish(mqtt_receive_topic, "off")
-
-# Once connected, subscribe to the MQTT topic
-mqtt_client.subscribe(mqtt_receive_topic)
-print("Connected and subscribed")
-
 # Publish a data point to the Adafruit IO MQTT server every 3 seconds
 # Note: Adafruit IO has rate limits in place, every 3 seconds is frequent
 #  enough to see data in realtime without exceeding the rate limit.
@@ -75,14 +66,9 @@ try:
         # Publish the data to the topic!
         print(f'Publish {sine:.2f}')
         mqtt_client.publish(mqtt_publish_topic, str(sine))
-        # Infinitely wait for messages on the topic.
-        # Note wait_msg() is a blocking call, if you're doing multiple things
-        # on the Pico you may want to look at putting this on another thread.
-        print(f'Waiting for messages on {mqtt_receive_topic}')
-        mqtt_client.wait_msg()
         
         # Delay a bit to avoid hitting the rate limit
-        #time.sleep(3)
+        time.sleep(3)
 except Exception as e:
     print(f'Failed to publish message: {e}')
 finally:
